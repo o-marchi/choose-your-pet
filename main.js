69f38ac8bda1ad11dee3ff66ad2956945b7b6301
Onelio de Marchi, Jr <oneliojr@gmail.com>
2022-06-15 23:08:57 -0300

    Add Oyama as my friend

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ console.log('It works');
 //             name: 'Marco',
 //         },
 //         {
-//             name: 'Roger',
+//             name: 'Oyama',
 //         },
 //     ],
 // };
@@ -20,47 +20,47 @@ console.log('It works');
 // console.log(marco.name);
 
 const animals = [
-    {
-        name: 'Dog',
-        image: 'https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png',
-    },
-    {
-        name: 'Cat',
-        image: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-    },
-    {
-        name: 'Iguana',
-        image: 'https://s1.static.brasilescola.uol.com.br/be/2022/05/iguana.jpg',
-    },
-    {
-        name: 'Parrot',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Papagaio-verdadeiro.jpg',
-    }
+  {
+    name: 'Dog',
+    image: 'https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png',
+  },
+  {
+    name: 'Cat',
+    image: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
+  },
+  {
+    name: 'Iguana',
+    image: 'https://s1.static.brasilescola.uol.com.br/be/2022/05/iguana.jpg',
+  },
+  {
+    name: 'Parrot',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Papagaio-verdadeiro.jpg',
+  }
 ];
 
 const menuElement = document.getElementById('menu');
 const imageElement = document.getElementById('image');
 
 function cleanImageElement() {
-    imageElement.innerHTML = '';
+  imageElement.innerHTML = '';
 }
 
 function createMenu() {
-    animals.forEach(animal => {
-        const button = document.createElement('button');
-        button.innerText = animal.name;
+  animals.forEach(animal => {
+    const button = document.createElement('button');
+    button.innerText = animal.name;
 
-        button.addEventListener('click', () => {
-            cleanImageElement();
+    button.addEventListener('click', () => {
+      cleanImageElement();
 
-            const image = document.createElement('img');
-            image.src = animal.image;
+      const image = document.createElement('img');
+      image.src = animal.image;
 
-            imageElement.appendChild(image);
-        });
-
-        menuElement.appendChild(button);
+      imageElement.appendChild(image);
     });
+
+    menuElement.appendChild(button);
+  });
 }
 
 createMenu();
